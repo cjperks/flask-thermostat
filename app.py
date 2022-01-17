995e3997892eceeb0af8d16b96409db0f571b68c
Chris Perkins <cjperks@gmail.com>
2022-01-17 20:57:04 +0000

    first basic working version with override

--- app.py
+++ app.py
@@ -1,10 +1,146 @@
-from flask import Flask
+from flask import Flask, g, render_template, request, redirect
+import sqlite3
+import datetime
+from datetime import timezone
+
+
 app = Flask(__name__)
 
+# some sort of database connection...
+DATABASE = 'thermostat.sqlite3'
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
+# Database connection
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+# Get records
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    # cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+# Write to db
+def write_db(query, args=()):
+    cur = get_db()
+    cur.execute(query, args)
+    print(query)
+    cur.commit()
+    # cur.close()
+
+
+# Homepage
+@app.route('/', methods=['POST', 'GET'])
+def home():
+
+    err_msg = ""
+
+    # if we're setting or cancelling an override
+    if request.method == "POST":
+        # set a new override
+        if 'override_expiry' in request.form:
+            try:
+                # write row into db
+                oTemp = request.form['override_temp']
+                oTime = request.form['override_expiry']
+                oTime = datetime.datetime.now().replace(
+                    hour=int(oTime[0:2]), minute=int(oTime[3:5]))
+                oStamp = query_db("SELECT datetime('now')", one=True)[0]
+                values = (str(oTime).split(".")[0], str(oTemp), oStamp)
+                write_db('INSERT INTO override (expires, temp, timestamp)'
+                    ' VALUES (?, ?, ?)', values)
+
+            except:
+                err_msg = "error creating the overrides"
+
+        # cancel an override
+        elif 'override_cancel' in request.form:
+            try:
+                write_db('DELETE FROM override;')
+
+            except:
+                err_msg = "error deleting the overrides"
+
+        else:
+            return "at least its a POST..."
+
+    # get the most current timestamp
+    dbd = query_db("SELECT datetime('now')")
+    server_now = datetime.datetime.strptime(dbd[0][0], '%Y-%m-%d %H:%M:%S')
+
+    # get the current boiler state
+    boiler_current = query_db('SELECT * FROM control_log '
+        'ORDER BY ROWID DESC', one=True)
+
+    # get the current setpoint
+    setpoint_current = query_db("SELECT * FROM schedule WHERE start_time "
+        "< time('now')", one=True)
+
+    # get the average temperature and check sensor status
+    rooms = query_db('SELECT * FROM location')
+    temp = 0
+    new_rooms = ()
+    for room in rooms:
+        roomtime = datetime.datetime.strptime(room[4], '%Y-%m-%d %H:%M:%S')
+        roomtime = roomtime.replace(tzinfo=datetime.timezone.utc)
+        tDiff = datetime.datetime.now(timezone.utc) - roomtime
+        # if the timestamp is older than 2 minutes, something might be wrong,
+        # so ignore and mark as error
+        if tDiff.seconds < 120:
+            new_rooms = new_rooms + (room + ("live",),)
+            temp = temp + room[3]
+
+        else:
+            new_rooms = new_rooms + (room + ("error",),)
+
+    temp = round(temp / len(rooms), 1)
+    rooms = new_rooms
+
+    # Get the override details
+    oride = query_db("SELECT * FROM override ORDER BY expires DESC", one=True)
+    # oride = dbd[0]
+
+    server_now = str(server_now.hour) + ":" + str(server_now.minute).zfill(2)
+
+    # Pass it all to the template
+    data = {'err_msg': err_msg,
+            'temp': temp,
+            'rooms': rooms,
+            'boiler_current': boiler_current,
+            'setpoint_current': setpoint_current,
+            'server_now': server_now,
+            'oride': oride}
+
+    # Render the page
+    return render_template('home.html', data=data)
+
+
+# Page to control the basic schedule
+@app.route('/schedule/')
+def schedule():
+    return 'This is where you set the schedule'
+
+
+# Page to display statistics about heating usage
+@app.route('/statistics/')
+def statistics():
+    return 'This is where the stats would be'
+
+
+# Main program
 if __name__ == '__main__':
+    app.debug = True
     app.run()
---
+++ thermostat.py
@@ -0,0 +1,273 @@
+# !/usr/bin/env python3
+
+# Changelog
+
+# v1.0
+# Single sensor input, single schedule, simple logging, no web interface
+
+# v1.1
+# Read any temp sensors that are availible
+
+# v1.2
+# interface with django db
+# auto add any new sensors to location table
+# inetgrate with web interface
+
+# v3
+# only record every 10 minutes
+
+
+# v4.0
+# changed record keeping to only record on temp or control output change
+# keep the last 5 readings in memory
+
+# Changelog end
+
+import time, os, signal, sys, sqlite3, datetime
+import RPi.GPIO as GPIO
+
+# set the General Purpose Input/Output pins to the correct mode
+GPIO.setmode(GPIO.BCM)
+
+# disable warnings - background process so they aren't visible - add to logging later?
+GPIO.setwarnings(False)
+
+# select the pin to control
+relay = 17
+
+# set the relay pin as an output pin
+GPIO.setup(relay, GPIO.OUT)
+
+
+# exit code
+def sigterm_handler(_signo, _stack_frame):
+    # switch off and free up pins
+    GPIO.cleanup()
+
+    # close db connection
+    conn.close()
+
+    print("Safe exit")
+
+    # exit program - I think??
+    sys.exit(0)
+
+
+# signal handler to catch the TERM signal
+signal.signal(signal.SIGTERM, sigterm_handler)
+
+signal.signal(signal.SIGINT, sigterm_handler)
+
+# connect to db for logging/interaction details
+conn = sqlite3.connect('/home/pi/boilerPi/thermostat.sqlite3')
+c = conn.cursor()
+
+sensorPath = "/sys/bus/w1/devices"
+
+# create the recent temperature matrix placeholder
+last5 = [[], []]
+
+# main section
+if __name__ == "__main__":
+
+    # loop forever
+    while True:
+
+        # get all the sensors
+        tempSensors = os.listdir(sensorPath)
+
+        # get data from all attached temperature sensors
+        # initialise array
+        sensorData = [[0 for j in range(3)] for i in range(len(tempSensors) - 1)]
+
+        # reset counter
+        i = 0
+        for sensor in tempSensors:
+
+            # ignore the sensor master
+            if sensor != "w1_bus_master1":
+
+                sensorData[i][0] = sensor
+
+                #  Open the sensor file
+                sensorFile = open(sensorPath + "/" + sensor + "/w1_slave")
+
+                #  Read all of the text in the file.
+                text = sensorFile.read()
+
+                #  Close the file now that the text has been read.
+                sensorFile.close()
+
+                # chop out the CRC section
+                sensorData[i][1] = text.split("\n")[0].split(" ")[11]
+
+                # grab the temperature value
+                sensorData[i][2] = float(text.split("\n")[1].split(" ")[9][2:]) / 1000
+
+                i += 1
+
+        # update the time as sqLite understands it
+        c.execute("SELECT datetime('now')")
+        timeStamp = c.fetchone()[0]
+
+        # update the current sensor inputs
+        for row in sensorData:
+
+            # check the CRC comes back ok
+            if row[1] == "YES":
+
+                # check if the sensor exists in the location table
+                c.execute("SELECT id from boilerWeb_location WHERE sensor = ?", (row[0],))
+
+                if c.fetchone() is None:
+                    # print("new location added")
+                    # add the location into the location table, if missing
+                    newLoc = "New location added " + str(datetime.datetime.now())
+                    c.execute("INSERT INTO boilerWeb_location (location_name, sensor, latest_temp, latest_time) VALUES(?,?,?,?)", (newLoc, row[0], row[2], timeStamp,))
+                    conn.commit()
+
+                    # add a temp long entry
+                    c.execute("INSERT INTO boilerWeb_temp_log (timestamp, sensor_id, temp) VALUES(?,?,?)", (timeStamp, row[0], row[2],))
+                    conn.commit()
+
+                else:
+                    # update the current temp record
+                    c.execute("UPDATE boilerWeb_location SET latest_time = ?, latest_temp = ? WHERE sensor = ?", (timeStamp, row[2], row[0],))
+                    # commit the record to the db
+                    conn.commit()
+
+                    # add an entry to the temperature log, if the temperature has changed
+                    c.execute("SELECT temp from boilerWeb_temp_log WHERE sensor_id = ? ORDER BY timestamp DESC LIMIT 1", (row[0],))
+
+                    lastTemp = c.fetchone()
+                    # check if there are any temp readings
+                    if lastTemp:
+                        # if there are readings, compare the last one
+                        if lastTemp[0] != row[2]:
+                            c.execute("INSERT INTO boilerWeb_temp_log (timestamp, sensor_id, temp) VALUES(?,?,?)", (timeStamp, row[0], row[2],))
+                            conn.commit()
+
+                    # if there are no logs at all
+                    else:
+                        # add a temp long entry
+                        c.execute("INSERT INTO boilerWeb_temp_log (timestamp, sensor_id, temp) VALUES(?,?,?)", (timeStamp, row[0], row[2],))
+                        conn.commit()
+
+        # print("Target temp : %f" %target)
+
+        # update the current sensor/reading matrix with current values
+        # 0 = sensor id
+        # 1 = CRC value
+        # 2 = temperature reading
+
+        # if we have no recent data, refil
+        if len(last5[0]) == 0:
+            for sensor in sensorData:
+                last5[0].append(sensor[0])
+                last5[1].append([sensor[2]])
+
+        # if we have some previous data
+        else:
+            for sensor in sensorData:
+                # check if the sensor is in the current list
+                if sensor[0] in last5[0]:
+                    # add the new reading to the start of the list
+                    si = last5[0].index(sensor[0])
+                    last5[1][si].insert(0,sensor[2])
+
+                    # keep only the most recent 5 readings
+                    last5[1][si] = last5[1][si][:5]
+
+                # add the sensor if it is not in the list already
+                else:
+                    last5[0].append(sensor[0])
+                    last5[1].append([sensor[2]])
+
+        # remove sensors that have broken/been removed from the system
+        for sensor in last5[0]:
+            if not any(sensor in i for i in sensorData):
+                last5[1].pop(last5[0].index(sensor))
+                last5[0].pop(last5[0].index(sensor))
+
+        # get the average for the last 5 mins, over all live sensors
+        i = 0
+        t = 0
+        for row in last5[1]:
+            for reading in row:
+                i += 1
+                t = t + reading
+
+        aveTemp = t / i
+        # print("Average temp = %f" % aveTemp)
+        # print(last5)
+
+        # check if we've changed state in the last 10 minutes
+        c.execute("SELECT output FROM boilerWeb_control_log WHERE timestamp >= (SELECT datetime('now', '-10 minutes')) ORDER BY timestamp DESC LIMIT 1")
+        lastOutput = c.fetchone()
+
+        # if there was a change inside 10 mins
+        if lastOutput:
+            # print("inside lockout period")
+            output = lastOutput[0]
+
+        # if there was not, we can decide what to do
+        else:
+            # get the override details if present and not expired
+            c.execute("SELECT temp FROM boilerWeb_override WHERE expires >= (SELECT datetime('now')) ORDER BY expires DESC LIMIT 1")
+            override = c.fetchone()
+
+            if override:
+                target = override[0]
+
+            else:
+                # get the normal scheduled temp
+                c.execute("SELECT min_temp FROM boilerWeb_schedule WHERE start_time <= (SELECT time('now', 'localtime')) ORDER BY start_time DESC LIMIT 1")
+                target = c.fetchone()[0]
+
+            # print("outside lockout")
+            if aveTemp < target:
+                output = 1
+
+            else:
+                output = 0
+
+        #
+        # ---IMPORTANT---
+        # frost protection override - this trumps all other temperature calculations
+        for row in sensorData:
+            if row[2] <= 7:
+                output = 1
+        # ---END IMPORTANT---
+        #
+
+        # write the record of the control output back to the db, if different to the existing
+        c.execute("SELECT output, timestamp FROM boilerWeb_control_log ORDER BY timestamp DESC LIMIT 1")
+        lastOutput = c.fetchone()
+
+        # prevents a crash if there is no log yet - shouldn't be an issue after initial config
+        if lastOutput:
+            # compare to last if there is a last
+            if output != lastOutput[0]:
+                # print("state change: {} -> {}" .format(lastOutput[0], output))
+                c.execute("INSERT INTO boilerWeb_control_log (timestamp, output) VALUES(?, ?)", (timeStamp, output))
+                conn.commit()
+
+            # else:
+                # print("no state change")
+
+        # if there is no last output - create one
+        else:
+            c.execute("INSERT INTO boilerWeb_control_log (timestamp, output) VALUES(?, ?)", (timeStamp, output))
+            conn.commit()
+
+        # print("Calculated state: %s" %output)
+
+        # turn the boiler on or off, depending on the decision
+        if output == 0:
+            GPIO.output(relay, True)
+
+        else:
+            GPIO.output(relay, False)
+
+        # sleep for 60 seconds
+        time.sleep(60)
